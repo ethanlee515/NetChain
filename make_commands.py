0749ed176aab0017c5809e87f50ad44799d62f6f
Nitish Paradkar <nitishparadkar@gmail.com>
2018-05-09 11:48:52 -0400

    fixed make_commands to generate a file per switch

--- make_commands.py
+++ make_commands.py
@@ -2,25 +2,37 @@
 
 import route
 
-locs = [[] for x in range(route.nb_switches)]
+commands = [list() for x in range(route.nb_switches)]
 
-if __name__ == "__main__":
-	with open("commands.txt", "w+") as f:
+with open("commands.txt", "w+") as f:
+	for key in range(route.max_key):
+		head, body, tail = route.get_chain(key)
+		p_head = "table_add process _put 2 1 " + str(head) + " " + str(key) + " => "
+		p_head += str(route.getLoc(head, key)) + " " + str(body)
+		commands[head - 1].append(p_head)
+		
+		p_body = "table_add process _put "
+		p_body += "2 1 " + str(body) + " " + str(key) + " => "
+		p_body += str(route.getLoc(body, key)) + " " + str(tail)
+		commands[body - 1].append(p_body)
+
+		locstr = str(route.getLoc(tail, key))
+
+		p_tail = "table_add process _put "
+		p_tail += "2 1 " + str(tail) + " " + str(key) + " => "
+		p_tail += locstr + " 0"
+		commands[tail - 1].append(p_tail)
+
+		g_tail = "table_add process _get "
+		g_tail += "2 0 " + str(tail) + " " + str(key) + " => "
+		g_tail += locstr + " 0"
+		commands[tail - 1].append(g_tail)
+
+for i in range(route.nb_switches):
+	with open("commands" + str(i + 1) + ".txt") as f:
 		f.write("table_set_default process _nop\n")
-		f.write("table_set_default route _route")
-		for key in range(route.max_key):
-			head, body, tail = route.get_chain(key)
-			f.write("\ntable_add process _put 2 1 " + str(head) + " " + str(key) + " => ")
-			f.write(str(route.getLoc(head, key)) + " " + str(body))
-			
-			f.write("\ntable_add process _put ")
-			f.write("2 1 " + str(body) + " " + str(key) + " => ")
-			f.write(str(route.getLoc(body, key)) + " " + str(tail))
+		f.write("table_set_default route _drop\n")
+		f.write("table_add route _route 2 =>")
+		for command in commands[i]:
+			f.write("\n" + command)
 
-			locstr = str(route.getLoc(tail, key))
-			f.write("\ntable_add process _put ")
-			f.write("2 1 " + str(tail) + " " + str(key) + " => ")
-			f.write(locstr + " 0")
-			f.write("\ntable_add process _get ")
-			f.write("2 0 " + str(tail) + " " + str(key) + " => ")
-			f.write(locstr + " 0")
