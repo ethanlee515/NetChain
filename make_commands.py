10f81f02550f79bb4bc61c8e7528b595545f39ff
Nitish Paradkar <nitishparadkar@gmail.com>
2018-05-09 12:20:05 -0400

    bugfix for make_commands.py

--- make_commands.py
+++ make_commands.py
@@ -4,29 +4,28 @@ import route
 
 commands = [list() for x in range(route.nb_switches)]
 
-with open("commands.txt", "w+") as f:
-	for key in range(route.max_key):
-		head, body, tail = route.get_chain(key)
-		p_head = "table_add process _put 2 1 " + str(head) + " " + str(key) + " => "
-		p_head += str(route.getLoc(head, key)) + " " + str(body)
-		commands[head - 1].append(p_head)
-		
-		p_body = "table_add process _put "
-		p_body += "2 1 " + str(body) + " " + str(key) + " => "
-		p_body += str(route.getLoc(body, key)) + " " + str(tail)
-		commands[body - 1].append(p_body)
+for key in range(route.max_key):
+	head, body, tail = route.get_chain(key)
+	p_head = "table_add process _put 2 1 " + str(head) + " " + str(key) + " => "
+	p_head += str(route.getLoc(head, key)) + " " + str(body)
+	commands[head - 1].append(p_head)
+	
+	p_body = "table_add process _put "
+	p_body += "2 1 " + str(body) + " " + str(key) + " => "
+	p_body += str(route.getLoc(body, key)) + " " + str(tail)
+	commands[body - 1].append(p_body)
 
-		locstr = str(route.getLoc(tail, key))
+	locstr = str(route.getLoc(tail, key))
 
-		p_tail = "table_add process _put "
-		p_tail += "2 1 " + str(tail) + " " + str(key) + " => "
-		p_tail += locstr + " 0"
-		commands[tail - 1].append(p_tail)
+	p_tail = "table_add process _put "
+	p_tail += "2 1 " + str(tail) + " " + str(key) + " => "
+	p_tail += locstr + " 0"
+	commands[tail - 1].append(p_tail)
 
-		g_tail = "table_add process _get "
-		g_tail += "2 0 " + str(tail) + " " + str(key) + " => "
-		g_tail += locstr + " 0"
-		commands[tail - 1].append(g_tail)
+	g_tail = "table_add process _get "
+	g_tail += "2 0 " + str(tail) + " " + str(key) + " => "
+	g_tail += locstr + " 0"
+	commands[tail - 1].append(g_tail)
 
 for i in range(route.nb_switches):
 	with open("commands" + str(i + 1) + ".txt", "w+") as f:
